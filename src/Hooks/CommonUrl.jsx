f9be7c072158af0abe889d21368e021333b777d2
Mdafsarx <mdafsar99009@gmail.com>
2024-06-10 22:33:34 +0600

    small bug solved

--- src/Hooks/CommonUrl.jsx
+++ src/Hooks/CommonUrl.jsx
@@ -4,8 +4,8 @@ import axios from "axios";
 const CommonUrl = () => {
 
     const axiosUrl = axios.create({
-        baseURL: 'http://localhost:1000',
-        // baseURL: import.meta.env.VITE_URL,
+        // baseURL: 'http://localhost:1000',
+        baseURL: import.meta.env.VITE_URL,
 
     });
     return axiosUrl
